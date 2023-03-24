2f50cd30c253f827520429391a1aef5acace8ae3
dan0115 <yecladaniel1@gmail.com>
2023-03-24 18:06:12 +0800

    retrieving my codes

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const dropdown = document.querySelector(".dropdown");
+// const dropdown = document.querySelector(".dropdown");
 
-dropdown.addEventListener("click", function() {
-  this.classList.toggle("active");
-});
+// dropdown.addEventListener("click", function() {
+//   this.classList.toggle("active");
+// });
 
-function validateForm() {
-  var x = document.forms["form"]["email"].value;
-  if (x == "") {
-    alert("Please fill out the form before submitting");
-    return false;
-  }
-}
+// function validateForm() {
+//   var x = document.forms["form"]["email"].value;
+//   if (x == "") {
+//     alert("Please fill out the form before submitting");
+//     return false;
+//   }
+// }
